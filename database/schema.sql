3dfddce8eb6521721a2c24ed47ab0c5cdc64cf94
NicolasLattanzi <lattanzi.1992502@studenti.uniroma1.it>
2024-03-24 12:31:03 +0100

    aggiustamenti al db, creato dominio String
    aggiustamenti al db, creato il dominio String e cambiato User in Customer

--- database/schema.sql
+++ database/schema.sql
@@ -1,37 +1,43 @@
+CREATE DOMAIN String AS VARCHAR(255);
 
-CREATE TABLE user (
+CREATE TYPE statusOrder AS ENUM ('not shipped', 'shipped', 'delivered');
+
+
+CREATE TABLE customer (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
-    mail VARCHAR(255) NOT NULL,
-    password VARCHAR (255) NOT NULL,
-    address VARCHAR (255) 
+    name String NOT NULL,
+    mail String NOT NULL,
+    password String NOT NULL,
+    address String 
 );
 
 CREATE TABLE seller (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name String NOT NULL,
     description TEXT,   
-    mail VARCHAR(255) NOT NULL,
-    password VARCHAR (255) NOT NULL
+    mail String NOT NULL,
+    password String NOT NULL
 );
 
 CREATE TABLE courier(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
-    mail VARCHAR(255) NOT NULL,
-    password VARCHAR (255) NOT NULL,
+    name String NOT NULL,
+    mail String NOT NULL,
+    password String NOT NULL,
     warehouse INTEGER NOT NULL,
+
     FOREIGN KEY (warehouse) REFERENCES warehouse (id) ON DELETE CASCADE 
 );
 
 CREATE TABLE product (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name String NOT NULL,
     description TEXT NOT NULL,
     quantity INTEGER NOT NULL,
     price_tag FLOAT NOT NULL,
     seller INTEGER NOT NULL,
     warehouse INTEGER NOT NULL,
+
     FOREIGN KEY (seller) REFERENCES seller (id) ON DELETE CASCADE,
     FOREIGN KEY (warehouse) REFERENCES warehouse (id) ON DELETE CASCADE,
     check(quantity >= 0)
@@ -39,8 +45,8 @@ CREATE TABLE product (
 
 CREATE TABLE warehouse (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR (255) NOT NULL,
+    name String NOT NULL,
+    address String NOT NULL,
 
 );
 
@@ -51,11 +57,12 @@ CREATE TABLE order (
     quantity INTEGER NOT NULL,
     date DATETIME NOT NULL,
     status statusOrder DEFAULT 'not shipped',
+
     FOREIGN KEY (user) REFERENCES user (id) ON DELETE CASCADE,
     FOREIGN KEY (courier) REFERENCES courier (id) ON DELETE CASCADE,
     FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE
 
-    );
+);
 
 CREATE TABLE wishList (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -68,6 +75,7 @@ CREATE TABLE wishList (
 CREATE TABLE addedProduct (
     product INTEGER NOT NULL,
     wishlist INTEGER NOT NULL, 
+
     PRIMARY KEY (product, wishlist),
     FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE,
     FOREIGN KEY (wishlist) REFERENCES wishlist (id) ON DELETE CASCADE
@@ -81,10 +89,9 @@ CREATE TABLE review (
     product INTEGER NOT NULL,
     stars INTEGER NOT NULL,
     description TEXT,
+
     FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE,
     FOREIGN KEY (user) REFERENCES user (id) ON DELETE CASCADE,
     check(stars >= 1 AND stars <= 5)
 
 );
-
-CREATE TYPE statusOrder AS ENUM ('not shipped', 'shipped', 'delivered');
