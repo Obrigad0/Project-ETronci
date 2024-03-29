48db6b71a19f8ac0f421401ba8c2b8c668086282
Flavio Corsetty <flavio.corsetti@yahoo.it>
2024-03-29 12:07:40 +0100

    inizio parte database

--- src/service/database/dbConnection.h
+++ src/service/database/dbConnection.h
@@ -6,35 +6,40 @@
 
 //extern "C"
 //{
-#include <libpq-fe.h> 
+#include <libpq-fe.h>  // libreria libpq, utilizzata per la connessione al db Postgres
 //}
 
 
-class Con2DB {
+class DbConnection {
 
 private:
  	    
-  PGconn *conn;
+  PGconn *dbConnection; // dbconnection contiene il puntatore alla connessione al database
+  // tramite dbconnection è possibile accedere alle informazioni sulla connessione e utilizzare le funzioni fornite dalla libreria libpq per interagire con il database
 
 public:
 
-/* connect to DB */ 
-  Con2DB(const char *hostname,
-	 const char *port,
-	 const char *username,
-	 const char *password,
-	 const char *dbname);  //  PGconn*  connect2db(); 
+  //Funzione che ci connette al database
+  connectToDatabase(char *hostname, char *port, char *username, char *password, char *dbname);  //  PGconn*  connect2db(); 
 
-  /* use this for commands returning no data, e.g. INSERT, UPDATE */
-  PGresult* ExecSQLcmd(char *sqlcmd);
+  /* use this for commands returning no data, e.g. INSERT, UPDATE */  // vecchia :)
+  //PGresult* ExecSQLcmd(char *sqlcmd); // vecchia :)
 
-/* use this for commands returning data, e.g. SELECT */
-  PGresult* ExecSQLtuples(char *sqlcmd);
+  // usata per eseguire Query che non ritornano dati. INSERT, UPDATE e DELETE
+  PGresult* RunActionQuery(char *query); //TO DO 
 
-  PGresult* RunQuery(char* query, bool is_tuples);
 
-  /* disconnect from DB */
-  void finish();
+  /* use this for commands returning data, e.g. SELECT */ // vecchia :)
+  // PGresult* ExecSQLtuples(char *sqlcmd); // vecchia :)
+
+  // usata per eseguire Query che ritornano dati. SELECT
+  PGresult* RunDataQuery(char *query); //TO DO
+
+
+  PGresult* RunQuery(char* query, bool is_tuples); // TO DO,  MICA HO CAPITO QUESTI PER COSA USANO STA FUNZIONE // vecchia
+
+  //Funzione che ci disconnette dal database
+  disconnectFromDatabase();
   
 };
 
