89cb3caa4834f391c49398db51f720cf35e3c786
Flavio Corsetty <flavio.corsetti@yahoo.it>
2024-05-14 19:06:38 +0200

    modificato schema
    miao miao miao

--- src/service/database/schema.sql
+++ src/service/database/schema.sql
@@ -1,58 +1,59 @@
 CREATE DOMAIN String AS VARCHAR(255);
 CREATE DOMAIN PartitaIva AS VARCHAR(11);
-CREATE TYPE statusOrder AS ENUM ('not shipped', 'shipped', 'delivered');
+CREATE TYPE StatusOrder AS ENUM ('not shipped', 'shipped', 'delivered');
 
 
-CREATE TABLE customer (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Customer (
+    id SERIAL PRIMARY KEY ,
     name String NOT NULL,
     mail String NOT NULL,
     password String NOT NULL,
-    address String 
+    UNIQUE(mail) 
 );
 
-CREATE TABLE seller (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Seller (
+    id SERIAL PRIMARY KEY ,
     name String NOT NULL,
     piva PartitaIva NOT NULL,
     description TEXT,
     mail String NOT NULL,
-    password String NOT NULL
+    password String NOT NULL,
+    UNIQUE(mail) 
+
 );
 
-CREATE TABLE courier(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Courier(
+    id SERIAL PRIMARY KEY ,
     name String NOT NULL,
     mail String NOT NULL,
     password String NOT NULL,
     warehouse INTEGER NOT NULL,
-
-    FOREIGN KEY (warehouse) REFERENCES warehouse (id) ON DELETE CASCADE 
+    FOREIGN KEY (warehouse) REFERENCES Warehouse(id) ON DELETE CASCADE,
+    UNIQUE(mail)
 );
 
 -- ho rimosso quantity e il check che sia maggiore di zero, meno problemi per noi
-CREATE TABLE product (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Product (
+    id SERIAL PRIMARY KEY,
     name String NOT NULL,
     description TEXT NOT NULL,
     price_tag FLOAT NOT NULL,
     seller INTEGER NOT NULL,
     warehouse INTEGER NOT NULL,
 
-    FOREIGN KEY (seller) REFERENCES seller (id) ON DELETE CASCADE,
-    FOREIGN KEY (warehouse) REFERENCES warehouse (id) ON DELETE CASCADE,
+    FOREIGN KEY (seller) REFERENCES Seller(id) ON DELETE CASCADE,
+    FOREIGN KEY (warehouse) REFERENCES Warehouse(id) ON DELETE CASCADE
 );
 
--- TO DO aggiungere la warehouse alla delivery dei corrieri (oppure far finta di niente e toglierla dal db?)
-CREATE TABLE warehouse (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS  Warehouse (
+    id SERIAL PRIMARY KEY,
     name String NOT NULL,
     address String NOT NULL
-
 );
 
-CREATE TABLE order (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- ex Ordine
+CREATE TABLE IF NOT EXISTS OrderedProduct (
+    id SERIAL PRIMARY KEY,
     customer INTEGER NOT NULL,
     product INTEGER NOT NULL,
     quantity INTEGER NOT NULL,
@@ -60,50 +61,48 @@ CREATE TABLE order (
     zip_code String NOT NULL,
     address String NOT NULL,
 
-    FOREIGN KEY (customer) REFERENCES customer (id) ON DELETE CASCADE,
-    FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE
-
+    FOREIGN KEY (customer) REFERENCES Customer(id) ON DELETE CASCADE,
+    FOREIGN KEY (product) REFERENCES Product(id) ON DELETE CASCADE,
+    CHECK (quantity > 0)
 );
 
-CREATE TABLE delivery (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS Delivery (
+    id SERIAL PRIMARY KEY,
+    orderId INTEGER NOT NULL, -- cambiato il nome, prima era order
     courier INTEGER NOT NULL,
-    date DATETIME NOT NULL,
-    status statusOrder DEFAULT 'not shipped',
+    date TIMESTAMP NOT NULL,
+    status StatusOrder DEFAULT 'not shipped',
 
-    FOREIGN KEY (order) REFERENCES order (id) ON DELETE CASCADE,
-    FOREIGN KEY (courier) REFERENCES courier (id) ON DELETE CASCADE,
+    FOREIGN KEY (orderId) REFERENCES OrderedProduct(id) ON DELETE CASCADE,
+    FOREIGN KEY (courier) REFERENCES Courier(id) ON DELETE CASCADE
 
 );
 
-CREATE TABLE wishList (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS WishList (
+    id SERIAL PRIMARY KEY,
     customer INTEGER NOT NULL,
-    FOREIGN KEY (customer) REFERENCES customer (id) ON DELETE CASCADE
+    FOREIGN KEY (customer) REFERENCES Customer(id) ON DELETE CASCADE
 
 );
-
--- prodotti aggiunti alla wishlist 
-CREATE TABLE addedProduct (
+--prodotto aggiunto alla wishlist
+CREATE TABLE IF NOT EXISTS AddedProduct (
     product INTEGER NOT NULL,
     wishlist INTEGER NOT NULL, 
 
     PRIMARY KEY (product, wishlist),
-    FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE,
-    FOREIGN KEY (wishlist) REFERENCES wishlist (id) ON DELETE CASCADE
+    FOREIGN KEY (product) REFERENCES Product(id) ON DELETE CASCADE,
+    FOREIGN KEY (wishlist) REFERENCES Wishlist(id) ON DELETE CASCADE
 
 );
 
-CREATE TABLE review (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Review (
+    id SERIAL PRIMARY KEY,
     customer INTEGER NOT NULL,
     product INTEGER NOT NULL,
     stars INTEGER NOT NULL,
     comment TEXT,
 
-    FOREIGN KEY (product) REFERENCES product (id) ON DELETE CASCADE,
-    FOREIGN KEY (customer) REFERENCES customer (id) ON DELETE CASCADE,
+    FOREIGN KEY (product) REFERENCES Product(id) ON DELETE CASCADE,
+    FOREIGN KEY (customer) REFERENCES Customer(id) ON DELETE CASCADE,
     check(stars >= 1 AND stars <= 5)
-
 );
